fa289abbd485122819ee5a36d6bef3303e2771cf
machadinhos <pbmachado2002@gmail.com>
2023-08-28 14:08:26 +0100

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
