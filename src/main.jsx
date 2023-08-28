c3415abbc89805947b418c4b450b67579da75162
machadinhos <pbmachado2002@gmail.com>
2023-08-28 14:10:51 +0100

    Code reformatted

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
+    <App/>
+</React.StrictMode>,);
