3f937a3a8ef079db2a647534345be4f6c9ebb01f
machadinhos <pbmachado2002@gmail.com>
2023-08-28 14:39:50 +0100

    App.jsx prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
 function App() {
-    return <></>;
+    return (<>
+        <Router>
+            <h1>App</h1>
+            <Routes>
+                <Route path="/" element={<p>Home</p>}/>
+                <Route path="/about" element={<p>About</p>}/>
+            </Routes>
+        </Router>
+    </>);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App.jsx';
 import './index.css';
+import ReactDOM from 'react-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
     <App/>
