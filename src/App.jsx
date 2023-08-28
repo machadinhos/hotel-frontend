7a1b1e8a2e81bc2be690ae164e25d4437ae7ceaf
machadinhos <pbmachado2002@gmail.com>
2023-08-28 17:14:56 +0100

    NaveBar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import NaveBar from './components/NaveBar/index.jsx';
 
 
 function App() {
     return (<>
         <Router>
-            <h1>App</h1>
+            <h1 className="title">Hotel Management system</h1>
+            <NaveBar/>
             <Routes>
-                <Route path="/" element={<p>Home</p>}/>
-                <Route path="/about" element={<p>About</p>}/>
+                <Route path="/" element={<h1>Home</h1>}/>
+                <Route path="/guests" element={<h1>Guests</h1>}/>
+                <Route path="/rooms" element={<h1>Rooms</h1>}/>
             </Routes>
         </Router>
     </>);
---
+++ src/components/NaveBar/index.jsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+const NaveBar = () => {
+    const handleClick = (url) => {
+        return function handleRedirect() {
+            window.location.href = url;
+        };
+    };
+
+    return (<div className="nave-bar">
+        <button onClick={handleClick('/')} className="nave-bar__button">Home</button>
+        <button onClick={handleClick('/guests')} className="nave-bar__button">Guests</button>
+        <button onClick={handleClick('/rooms')} className="nave-bar__button">Rooms</button>
+    </div>);
+};
+
+export default NaveBar;
